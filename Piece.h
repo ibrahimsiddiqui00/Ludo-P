9d9b1dbb02e20b08bdcc6080e8cf4f7c40632072
ibrahimsiddiqui00 <64732341+ibrahimsiddiqui00@users.noreply.github.com>
2020-05-05 20:52:52 +0500

    UpdatedPiece.h

--- Piece.h
+++ Piece.h
@@ -9,22 +9,10 @@ protected:
 	bool HasKilled;
 	int Clr;	//optional can be removed
 public:
-	Piece(int C, Team* T) : Clr(C), CT(T), OutofBase(false), HasKilled(false)
-	{
-
-	}
+	Piece(int C, Team* T);
 	void Draw(Point P);		//this will draw a particular piece
-	bool IsMyPiece(Team T)
-	{
-		/*for (int i{ 0 }; i < T.Size(); i++)
-		{
-			if(Clr==T.PColor(i))
-		}*/
-		return &T == CT;
-	}
-	bool IsMyPiece(Player P);
-	//this function will check whether this is the team's piece or not 	
-	void Reset();		//when a piece is killed , it will be reset 
+	bool IsMyPiece(Team T);  //this function will check whether this is the team's piece or not
+	bool IsMyPiece(Player P); //this function will check whether this is the player's piece or not 	
+	void Reset(Point E);		//when a piece is killed , it will be reset 
 
 };
-
